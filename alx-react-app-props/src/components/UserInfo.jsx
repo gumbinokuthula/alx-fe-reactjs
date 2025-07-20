a50309eecbaed19afd9343d359ad15b787357c87
Nokuthula Gumbi <nokuthulagumbi737@gmail.com>
2025-07-20 20:14:43 +0200

    fix Context API implementation and file naming

---
+++ alx-react-app-props/src/components/UserInfo.jsx
@@ -0,0 +1,8 @@
+// src/components/UserInfo.jsx
+import UserProfile from './UserProfile';
+
+function UserInfo() {
+  return <UserProfile />;
+}
+
+export default UserInfo;
--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,13 +1,16 @@
+// src/components/UserProfile.jsx
+import { useContext } from 'react';
+import UserContext from '../UserContext';
 
-function UserProfile(props) {
+function UserProfile() {
+  const userData = useContext(UserContext);
+
   return (
-    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px', borderRadius: '8px' }}>
-      <h2 style={{ color: 'blue', fontSize: '24px' }}>{props.name}</h2>
-      <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
-      <p>Bio: {props.bio}</p>
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
     </div>
   );
 }
 
 export default UserProfile;
-
