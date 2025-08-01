0d0bd9cef499f44d990ff9d81361dd6ca11b6e28
Nokuthula Gumbi <nokuthulagumbi737@gmail.com>
2025-08-01 21:48:37 +0200

    Add advanced search and filtering to recipe app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,28 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm';
-import EditRecipeForm from './components/EditRecipeForm';
-import RecipeDetail from './components/RecipeDetail'; // ✅ NEW IMPORT
 import SearchBar from './components/SearchBar';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <h1>Recipe Sharing App</h1>
-        <SearchBar />
-
-        <Routes>
-          <Route path="/" element={<RecipeList />} />
-          <Route path="/add" element={<AddRecipeForm />} />
-          <Route path="/edit/:id" element={<EditRecipeForm />} />
-          <Route path="/recipe/:id" element={<RecipeDetail />} /> {/* ✅ NEW ROUTE */}
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <h1>Recipe Sharing App</h1>
+      <SearchBar />
+      <RecipeList />
+    </div>
   );
 }
-
-export default App;
