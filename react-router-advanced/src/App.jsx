9dd8ccd64629a32c1930efc9e19373a7af3768d5
Nokuthula Gumbi <nokuthulagumbi737@gmail.com>
2025-08-24 11:06:08 +0200

    feat: implement advanced routing with nested, dynamic, and protected routes

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,11 +1,38 @@
+// src/App.jsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 import Profile from "./components/Profile";
+import ProfileDetails from "./components/ProfileDetails";
+import ProfileSettings from "./components/ProfileSettings";
+import BlogPost from "./components/BlogPost";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-// ... inside <Routes>
-<Route
-  path="/profile/*"
-  element={
-    <ProtectedRoute>
-      <Profile />
-    </ProtectedRoute>
-  }
-/>
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Public Home route */}
+        <Route path="/" element={<Home />} />
+
+        {/* Protected Profile route with nested routes */}
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        >
+          {/* Nested routes inside Profile */}
+          <Route path="details" element={<ProfileDetails />} />
+          <Route path="settings" element={<ProfileSettings />} />
+        </Route>
+
+        {/* Dynamic blog post route */}
+        <Route path="/blog/:id" element={<BlogPost />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
